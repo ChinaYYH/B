556dc03fef4bd3d421e98e764f69327b19e67bd5
ChinaYYH <1632463212@qq.com>
2023-09-18 19:24:41 +0800

    feat(package.json): 登陆页面布局

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+console.log(useRoute())
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from '../views/Login/index.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
+
+export default router
